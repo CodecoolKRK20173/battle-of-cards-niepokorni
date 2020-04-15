a5a12e3a0582ddaa6a7ba4ec61f0a5ec80c88e2b
Radek Szczepanik <radekszczepanik.rsz@gmail.com>
2020-04-15 08:02:42 +0200

    update SingleCardView file

--- SingleCardView.cs
+++ SingleCardView.cs
@@ -32,12 +32,12 @@ namespace CardGame
             Console.WriteLine(
                  
 $@"                         __________________                                   
-                      __|________________  |                                    
+                      __|________________  |                                                Wybierz na co chcesz walczyć:
                    __|_________________  | |                                    
-                  |Player: {name.PadRight(8)}    | | |                                      
-                  |                    | | |                                    
-                  |   {cardName.PadRight(15)}  | | |                        .------.    
-                  |                    | | |     .------.           |S .   |    
+                  |Player: {name.PadRight(8)}    | | |                                                1. Engine Cap {EC}
+                  |                    | | |                                                2. Max Speed {MS}
+                  |   {cardName.PadRight(15)}  | | |                        .------.                3. Acceleration {AC}
+                  |                    | | |     .------.           |S .   |                4. Engine Power {EP}
                   |   EngineCap: {EC.PadRight(4)}  | | |     |C_  _ |    .------; / \  |    
                   |   MaxSpeed: {MS.PadRight(4)}   | | |     |( \/ )|-----. _   |(_,_) |    
                   | Acceleration: {AC.PadRight(4)} | | |     | \  / | /\  |( )  |  I  S|    
@@ -51,6 +51,58 @@ $@"                         __________________
                   |_______C.A.R.S______| "
                  
                  );
+        Player player = new Player();
+        GameInterface game = new GameInterface();
+
+            Console.WriteLine();
+            Console.Write("Choose option: ");
+
+            string choice = Console.ReadLine();
+
+            if(choice == "1")
+            {
+                Console.WriteLine(EC);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(player.SingleCardOnPlayerHand.EngineCapacity);
+                Console.ResetColor();
+                if (int.Parse(EC) > int.Parse(player.SingleCardOnPlayerHand.EngineCapacity))
+                {
+                    Console.WriteLine($"{name} " + "wins");
+                    PrintPlayerCard();
+                }
+                else
+                {
+                    Console.WriteLine($"Ola " + "wins");
+                    PrintPlayerCard();
+                }
+                
+            }
+            else if(choice == "2")
+            {
+                Console.WriteLine(MS);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(player.SingleCardOnPlayerHand.MaxSpeed);
+                Console.ResetColor();
+                if (int.Parse(MS) > int.Parse(player.SingleCardOnPlayerHand.MaxSpeed))
+                {
+                    Console.WriteLine();
+                }
+            }
+            else if(choice == "3")
+            {
+                Console.WriteLine(AC);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(player.SingleCardOnPlayerHand.Acceleration);
+                Console.ResetColor();
+            }
+            else if(choice == "4")
+            {
+                Console.WriteLine(EP);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine(player.SingleCardOnPlayerHand.EnginePower);
+                Console.ResetColor();
+            }
+     
         }
     }        
 }
